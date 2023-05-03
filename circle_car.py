81deb2af914b30a36e32b0a626bcd38200ebc0d3
hj-1006 <65325008+hj-1006@users.noreply.github.com>
2023-05-03 09:39:01 +0900

    Add files via upload

---
+++ circle_car.py
@@ -0,0 +1,22 @@
+import turtle
+t =turtle.Turtle()
+t.shape("turtle")
+
+
+t.forward(300)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(300)
+t.left(90)
+t.forward(100)
+
+t.up()
+t.goto(0,0)
+t.down()
+t.circle(50)
+t.up()
+t.goto(200,0)
+t.down()
+t.circle(50)
+turtle.done()
