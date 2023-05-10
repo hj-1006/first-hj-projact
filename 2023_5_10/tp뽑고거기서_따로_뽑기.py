4aa69279e753a996f076d3b2a26d4d99bc642aa0
hj-1006 <65325008+hj-1006@users.noreply.github.com>
2023-05-10 12:13:57 +0900

    Add files via upload

--- 2023_5_10/tp뽑고거기서_따로_뽑기.py
+++ 2023_5_10/tp뽑고거기서_따로_뽑기.py
@@ -5,8 +5,9 @@ load_url = "https://news.daum.net/society#1"
 html = requests.get(load_url)
 soup = BeautifulSoup(html.content,"html.parser")
 
-#링크뽑기
-for el in soup.find_all("a"):
+#topic 으로 뽑기
+topic = soup.find(class_="price_stock")
+for el in topic.find_all("a"):
     print(el.text)
     url = el.get("href")
     print(url)
