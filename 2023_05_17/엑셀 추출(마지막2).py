086c94362603b71c19741128ceee1cbcb1a9580d
hj-1006 <65325008+hj-1006@users.noreply.github.com>
2023-05-24 12:37:18 +0900

    Add files via upload

--- 2023_05_17/엑셀 추출(마지막2).py
+++ 2023_05_17/엑셀 추출(마지막2).py
@@ -6,3 +6,5 @@ df = pd.read_csv("test.csv",encoding="mbcs")
 kor = df.sort_values("국어",ascending=False)
 
 kor.to_excel("csv_to_excell.xlsx")
+
+kor.to_excel("csv_to_excell2.xlsx",index=False,sheet_name="국어로 정렬")
